35f29c098619964f4af7a9b1721de02f288af29f
Soren <soren.starck@etu.umontpellier.fr>
2023-09-19 22:28:01 +0200

    version de base avec structure MVC

---
+++ src/Controller/ControllerMain.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\SAE\Controller;
+
+use App\SAE\Model\ModelSAE;
+
+class ControllerMain
+{
+    public static function home()
+    {
+        self::afficheVue(
+                    'view.php',
+                    [
+                        'pagetitle' => 'Accueil',
+                        'cheminVueBody' => 'SAE/home.php',
+                    ]
+                );
+    }
+
+    private static function afficheVue(string $cheminVue, array $parametres = []): void
+    {
+        extract($parametres); // Crée des variables à partir du tableau $parametres
+        require __DIR__ . '/../View/' . $cheminVue;
+    }
+}
+
+?>
