5cb03a59755cbcfa8447eba42ef4745d5a50d0af
Soren <soren.starck@etu.umontpellier.fr>
2023-09-24 10:35:24 +0200

    footer working

--- src/View/view.php
+++ src/View/view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $pagetitle; ?></title>
-    <link rel="stylesheet" href="assets/css/style.css">
+
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="assets/css/charte-graphique-UM.css">
 </head>
