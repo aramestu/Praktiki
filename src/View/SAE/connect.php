b01c4d80fd6ada2daf49a9812fcecbdebafbd03c
Soren <soren.starck@etu.umontpellier.fr>
2023-09-24 11:35:29 +0200

    connexion compte

--- src/View/SAE/connect.php
+++ src/View/SAE/connect.php
@@ -1,33 +1,47 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="assets/css/connect.css">
+</head>
+
 <body>
-<div class="container">
-<form method="get" action="../web/frontController.php">
-    <fieldset>
-        <legend>Mon formulaire :</legend>
-        <p>
-            <label for="immat_id">Immatriculation</label> :
-            <input type="text" placeholder="256AB34" name="immat" id="immat_id" required/>
-        </p>
-        <p>
-            <label for="marque_id">Marque</label> :
-            <input type="text" placeholder="renault" name="marque" id="marque_id" required/>
-        </p>
-        <p>
-            <label for="couleur_id">Couleur</label> :
-            <input type="text" placeholder="bleu" name="couleur" id="couleur_id" required/>
-        </p>
-        <p>
-            <label for="nbSieges_id">Nombre de places</label> :
-            <input type="number" placeholder="5" name="nbSieges" id="nbSieges_id" required/>
-        </p>
-        <p>
-            <input type="submit" value="Envoyer" />
-            <input type='hidden' name='action' value='connected'>
-        </p>
-    </fieldset>
-</form>
-</div>
+    <div class="container">
+        <form method="post" action="login.php">
+                <legend>Connexion</legend>
+                <p>
+                    <label for="username">Email</label>
+                    <input type="text" name="username" id="username" required placeholder="rick.astley@roll.com"/>
+                </p>
+                <p>
+                    <label for="password">Mot de passe</label>
+                    <div class="password-input">
+                        <input type="password" name="password" id="password" required placeholder="mot de passe"/>
+                        <button type="button" id="showPassword"><img id="showPasswordIcon" src="assets/images/eye-icon.png" alt="O" /></button>
+                    </div>
+                </p>
+                <p>
+                    <input type="submit" value="Connect" />
+                </p>
+        </form>
+        <div class="create-account">
+            <p>Vous n'avez pas de compte? <a href="create_account.php">Créer un compte</a></p>
+        </div>
+    </div>
+    <script>
+        const passwordInput = document.getElementById('password');
+        const showPasswordButton = document.getElementById('showPassword');
+
+        showPasswordButton.addEventListener('click', () => {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+            } else {
+                passwordInput.type = 'password';
+            }
+        });
+    </script>
 </body>
+
 </html>
