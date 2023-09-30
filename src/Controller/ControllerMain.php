02850ce805a88aff9f9ffdbf6eeced4f54d74b1a
0CT0PUCE <115957989+0CT0PUCE-LOG@users.noreply.github.com>
2023-09-30 11:14:41 +0200

    refractor js for view.php

--- src/Controller/ControllerMain.php
+++ src/Controller/ControllerMain.php
@@ -51,7 +51,7 @@ class ControllerMain
 
     private static function afficheVue(string $cheminVue, array $parametres = []): void
     {
-        extract($parametres); // Crée des variables à partir du tableau $parametres
+        extract($parametres);
         require __DIR__ . '/../View/' . $cheminVue;
     }
 }
