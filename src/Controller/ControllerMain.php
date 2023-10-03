77c66a110e625c631c7ff00b404a47cf53777e9b
Audouy Thibaut <thibaut.audouy@etu.umontpellier.fr>
2023-10-03 18:14:54 +0200

    Ajout d'un bouton dans home.php qui redirige vers la modification d'offre mais qui fonctionnera en passant un id d'offre via l'URL (temp). Ajout de méthodes pour gérer le formulaire de modification dans le ControllerMain.php

--- src/Controller/ControllerMain.php
+++ src/Controller/ControllerMain.php
@@ -91,10 +91,23 @@ class ControllerMain
         }
     }
 
+    // EN COURS
+    // Prend par défaut un Stage ou une Alternance
     public static function afficherFormulaireModification(){
+        $expPro = $_GET["experiencePro"];
+        $pagetitle = 'Modification Offre';
+        $cheminVueBody = 'SAE/editOffer.php';
+        if(get_class($expPro) == "Alternance"){
 
+        }
+        ControllerMain::afficheVue("editOffer.php", [
+            "pagetitle" => 'Modification Offre',
+            "cheminVueBody" => 'SAE/editOffer.php',
+            "experiencePro" => $_GET["id"]
+        ]);
     }
 
+    // EN COURS
     public static function modifierDepuisFormulaire(){
 
     }
