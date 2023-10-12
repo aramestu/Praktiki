441ebf1b48393623b9ab30c41fa1222be07be591
thiba <thibaut.audouy@etu.umontpellier.fr>
2023-10-12 19:46:07 +0200

    Réecriture code endOffer.php plus propre

--- src/View/SAE/endOffer.php
+++ src/View/SAE/endOffer.php
@@ -1,11 +1,17 @@
+<head>
+    <link rel="stylesheet" href="assets/css/button.css">
+</head>
 <div class="container">
     <h1><?php echo $message; ?></h1>
     <br>
     <br>
-    <!-- Je sais c'est moche, je modifierai plus tard le code, je suis nul en css -->
+    <p>
+        <a href="frontController.php?action=home"><button type="button" class="btn btn-primary">Retourner à l'accueil</button></a>
+    </p>
+    <!-- Je sais c'est moche, je modifierai plus tard le code, je suis nul en css
     <form action="frontController.php?action=home">
         <input type="submit" value="Retourner à l'accueil"/>
-    </form>
+    </form>-->
 </div>
 
 </html>
