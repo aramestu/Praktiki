8eeda720bebd205076309b671b58f7cd755cb135
le-clems <clementhamel05@gmail.com>
2023-10-12 20:09:33 +0200

    Front pour les offres de la liste version 2

--- src/View/SAE/smallOffer.php
+++ src/View/SAE/smallOffer.php
@@ -1,20 +1,22 @@
 <div class="subContainer small">
+    <div class="header">
+        <div class="left">
+            <p class="bold"><?php
+                $full_path = get_class($expPro);
+                $elements = explode('\\', $full_path);
+                $last_element = end($elements);
+                echo $last_element
+                ?></p>
+            <p>Date du poste</p>
+        </div>
+        <div class="right">
+            <p>Du <?php echo $expPro->getDateDebut();?></p>
+            <p>au <?php echo $expPro->getDateFin();?></p>
+        </div>
+    </div>
     <div class="information">
-        <p class="bold"><?php
-                                        $full_path = get_class($expPro);
-                                        $elements = explode('\\', $full_path);
-                                        $last_element = end($elements);
-                                        echo $last_element
-                                        ?></p>
-        <p>Date du poste</p>
-
-    </div>
-    <div class="company">
-        <h2>Entreprise</h2>
+        <h3><?php echo $expPro->getSujet();?></h3>
+        <p>Entreprise</p>
         <label><?php echo $expPro->getAdresse();?></label>
-    </div>
-    <div class="information2">
-        <p>du date <?php echo $expPro->getDateDebut();?></p>
-        <p>au date <?php echo $expPro->getDateFin();?></p>
     </div>
 </div>
