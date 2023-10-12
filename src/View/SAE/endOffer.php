08ba50f8f36b75b16589c11868fdae65c513e7c5
thiba <thibaut.audouy@etu.umontpellier.fr>
2023-10-12 18:51:33 +0200

    Nouvel Page après modif ou création d'offre

---
+++ src/View/SAE/endOffer.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <h1><?php echo $message; ?></h1>
+    <br>
+    <br>
+    <!-- Je sais c'est moche, je modifierai plus tard le code, je suis nul en css -->
+    <form action="frontController.php?action=home">
+        <input type="submit" value="Retourner à l'accueil"/>
+    </form>
+</div>
+
+</html>
