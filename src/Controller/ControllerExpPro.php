4d06e3c665019012a7bcbaa41e3d8b11022c44e6
Norman Francois <norman.francois@etu.umontpellier.fr>
2023-10-15 01:25:33 +0200

    résolution de bug dans le ControllerExpPro.php

--- src/Controller/ControllerExpPro.php
+++ src/Controller/ControllerExpPro.php
@@ -36,20 +36,6 @@ class ControllerExpPro extends ControllerGenerique{
         );
     }
 
-    public static function getExpProBySearch(){
-        $keywords = urldecode($_GET['keywords']);
-        $listeExpPro = ExperienceProfessionnelRepository::search($keywords);
-        self::afficheVue(
-            'view.php',
-            [
-                'pagetitle' => 'Offre',
-                'listeExpPro' => $listeExpPro,
-                'keywords' => $keywords,
-                'cheminVueBody' => 'offer/offerList.php',
-            ]
-        );
-    }
-
     public static function getExpProByFiltre(): void
     {
         $dateDebut = null;
