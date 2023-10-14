bfe2888034bc79624986744d5291ebb41609994d
thiba <thibaut.audouy@etu.umontpellier.fr>
2023-10-14 13:49:22 +0200

    Correction formulaire création stage / Alternance

--- web/assets/javascript/showHideToggle.js
+++ web/assets/javascript/showHideToggle.js
@@ -2,13 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const typeOffre = document.getElementById('typeOffre');
     const stageForm = document.getElementById('stageForm');
     const alternanceForm = document.getElementById('alternanceForm');
+    const gratification = document.getElementById('gratification');
 
     // Fonction pour masquer le formulaire non sélectionné
     function toggleFormDisplay() {
+        // Supprimer l'attribut "required" de l'élément gratification
+        //gratification.removeAttribute('required');
+
+
         if (typeOffre.value === 'stage') {
+            gratification.setAttribute('required', 'required');
             stageForm.classList.remove('hidden');
             alternanceForm.classList.add('hidden');
         } else if (typeOffre.value === 'alternance') {
+            gratification.removeAttribute('required');
             stageForm.classList.add('hidden');
             alternanceForm.classList.remove('hidden');
         }
