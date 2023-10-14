76c31537092f2b5d89aa820a6f6ab1dd389508ee
Soren Starck <soren.starck@etu.umontpellier.fr>
2023-10-14 20:53:54 +0200

    résolution problème bouton search

--- src/View/offer/offerTable.php
+++ src/View/offer/offerTable.php
@@ -4,37 +4,4 @@ foreach($listeExpPro as $expPro){
     require 'smallOffer.php';
 }
 ?>
-<!--
-      <table class="table table-bordered" style="border:none;">
-       <thead>
-         <tr>
-         </tr>
-       </thead>
-       <tbody>
-         <?php
-           $rank = 1;
-           $cellCount = 0;
-           foreach ($listeExpPro as $expPro) {
-             if ($cellCount === 0) {
-               echo '<tr>';
-             }
-             ?>
-             <td class="session-cell" data-session-id="<?php echo $data['idSession']; ?>">
-               <?php require 'smallOffer.php'; ?>
-             </td>
-             <?php
-             $cellCount++;
-             if ($cellCount === 4) {
-               echo '</tr>';
-               $cellCount = 0;
-             }
-           }
-           if ($cellCount > 0) {
-             echo str_repeat('<td colspan="3"></td>', 3 - $cellCount) . '</tr>';
-           }
-            ?>
-
-       </tbody>
-     </table>
-     -->
-   </div>
+</div>
