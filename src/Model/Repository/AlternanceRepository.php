c34cbb62c03ae28a2d8e5bd262c804ce160f0f34
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2023-10-15 03:11:35 +0200

    Remise en place de la fonction search

--- src/Model/Repository/AlternanceRepository.php
+++ src/Model/Repository/AlternanceRepository.php
@@ -4,9 +4,11 @@ namespace App\SAE\Model\Repository;
 
 use App\SAE\Model\DataObject\Alternance;
 
-class AlternanceRepository{
-    public static function save(Alternance $a): bool {
-        try{
+class AlternanceRepository
+{
+    public static function save(Alternance $a): bool
+    {
+        try {
             ExperienceProfessionnelRepository::save($a);
             $pdo = Model::getPdo();
             $requestStatement = $pdo->prepare("INSERT INTO Alternances(idAlternance)
@@ -15,43 +17,45 @@ class AlternanceRepository{
 
             $requestStatement->execute($values);
             return true;
-        }catch(\PDOException $e){
+        } catch (\PDOException $e) {
             return false;
         }
     }
 
-    public static function construireDepuisTableau($alternanceFormatTableau): Alternance {
+    public static function construireDepuisTableau($alternanceFormatTableau): Alternance
+    {
         $alternance = new Alternance($alternanceFormatTableau["sujet"], $alternanceFormatTableau["thematique"], $alternanceFormatTableau["taches"], $alternanceFormatTableau["codePostal"], $alternanceFormatTableau["adresse"], $alternanceFormatTableau["dateDebut"], $alternanceFormatTableau["dateFin"], $alternanceFormatTableau["siret"]);
-        if(array_key_exists("idAlternance", $alternanceFormatTableau)){
-            if(!empty($alternanceFormatTableau["idAlternance"])){
+        if (array_key_exists("idAlternance", $alternanceFormatTableau)) {
+            if (!empty($alternanceFormatTableau["idAlternance"])) {
                 $alternance->setId($alternanceFormatTableau["idAlternance"]);
             }
         }
-        if(array_key_exists("etudiant", $alternanceFormatTableau)){
-            if(!empty($alternanceFormatTableau["etudiant"])){
+        if (array_key_exists("etudiant", $alternanceFormatTableau)) {
+            if (!empty($alternanceFormatTableau["etudiant"])) {
                 $alternance->setEtudiant($alternanceFormatTableau["etudiant"]);
             }
         }
-        if(array_key_exists("enseignant", $alternanceFormatTableau)){
-            if(!empty($alternanceFormatTableau["enseignant"])){
+        if (array_key_exists("enseignant", $alternanceFormatTableau)) {
+            if (!empty($alternanceFormatTableau["enseignant"])) {
                 $alternance->setEnseignant($alternanceFormatTableau["enseignant"]);
             }
 
         }
-        if(array_key_exists("tuteurProfessionnel", $alternanceFormatTableau)){
-            if(!empty($alternanceFormatTableau["tuteurProfessionnel"])){
+        if (array_key_exists("tuteurProfessionnel", $alternanceFormatTableau)) {
+            if (!empty($alternanceFormatTableau["tuteurProfessionnel"])) {
                 $alternance->setTuteurProfessionnel($alternanceFormatTableau["tuteurProfessionnel"]);
             }
         }
-        if(array_key_exists("datePublication", $alternanceFormatTableau)){
-            if(!empty($alternanceFormatTableau["datePublication"])){
+        if (array_key_exists("datePublication", $alternanceFormatTableau)) {
+            if (!empty($alternanceFormatTableau["datePublication"])) {
                 $alternance->setDatePublication($alternanceFormatTableau["datePublication"]);
             }
         }
         return $alternance;
     }
 
-    public static function getAll() : array{
+    public static function getAll(): array
+    {
         $pdo = Model::getPdo();
         $requestStatement = $pdo->query(" SELECT idAlternance, sujetExperienceProfessionnel AS sujet, thematiqueExperienceProfessionnel AS thematique, tachesExperienceProfessionnel AS taches,
                                                 codePostalExperienceProfessionnel AS codePostal, adresseExperienceProfessionnel AS adresse, dateDebutExperienceProfessionnel AS dateDebut,
@@ -59,13 +63,14 @@ class AlternanceRepository{
                                                 FROM ExperienceProfessionnel e
                                                 JOIN Alternances a ON a.idAlternance = e.idExperienceProfessionnel");
         $AllAlternance = [];
-        foreach ($requestStatement as $alternanceTab){
+        foreach ($requestStatement as $alternanceTab) {
             $AllAlternance[] = self::construireDepuisTableau($alternanceTab);
         }
         return $AllAlternance;
     }
 
-    public static function get(string $id) :?Alternance{
+    public static function get(string $id): ?Alternance
+    {
         $sql = "SELECT idAlternance, sujetExperienceProfessionnel AS sujet, thematiqueExperienceProfessionnel AS thematique, tachesExperienceProfessionnel AS taches,
                 codePostalExperienceProfessionnel AS codePostal, adresseExperienceProfessionnel AS adresse, dateDebutExperienceProfessionnel AS dateDebut,
                 dateFinExperienceProfessionnel AS dateFin, siret, datePublication, numEtudiant AS etudiant, mailEnseignant AS enseignant, mailTuteurProfessionnel AS tuteurProfessionnel
@@ -83,7 +88,7 @@ class AlternanceRepository{
         $alternance = $pdoStatement->fetch();
 
         // S'il n'y a pas d'alternance
-        if (! $alternance) {
+        if (!$alternance) {
             return null;
         } else {
             return AlternanceRepository::construireDepuisTableau($alternance);
@@ -110,31 +115,33 @@ class AlternanceRepository{
         ExperienceProfessionnelRepository::supprimer($alternance);
     }
 
-    public static function filtre(string $dateDebut = null, string $dateFin = null, string $optionTri = null, string $codePostal = null, string $datePublication = null) : array{
+    public static function filtre(string $dateDebut = null, string $dateFin = null, string $optionTri = null, string $codePostal = null, string $datePublication = null): array
+    {
         date_default_timezone_set('Europe/Paris');
         $pdo = Model::getPdo();
         $sql = "SELECT idAlternance, sujetExperienceProfessionnel AS sujet, thematiqueExperienceProfessionnel AS thematique, tachesExperienceProfessionnel AS taches,
                 codePostalExperienceProfessionnel AS codePostal, adresseExperienceProfessionnel AS adresse, dateDebutExperienceProfessionnel AS dateDebut,
-                dateFinExperienceProfessionnel AS dateFin, siret, numEtudiant AS etudiant, mailEnseignant AS enseignant, mailTuteurProfessionnel AS tuteurProfessionnel, datePublication FROM Alternances a JOIN ExperienceProfessionnel e ON a.idalternance = e.idExperienceProfessionnel ";
-        if (isset($datePublication)){
-            $sql .= match ($datePublication){
+                dateFinExperienceProfessionnel AS dateFin, siret, numEtudiant AS etudiant, mailEnseignant AS enseignant, mailTuteurProfessionnel AS tuteurProfessionnel, datePublication FROM Alternances a JOIN ExperienceProfessionnel e ON a.idalternance = e.idExperienceProfessionnel WHERE numEtudiant IS NULL ";
+        if (isset($datePublication)) {
+            $sql .= match ($datePublication) {
                 'last24' => "AND DATEDIFF(NOW(), datePublication) < 1 ",
                 'lastWeek' => "AND DATEDIFF(NOW(), datePublication) < 7 ",
                 'lastMonth' => "AND DATEDIFF(NOW(), datePublication) < 30 ",
             };
         }
-        if (strlen($dateDebut) > 0){
+        if (strlen($dateDebut) > 0 && strlen($dateFin) > 0) {
+            $sql .= "AND dateDebutExperienceProfessionnel >= $dateDebut AND dateFinExperienceProfessionnel <= $dateFin ";
+        } elseif (strlen($dateDebut) > 0) {
             $sql .= "AND dateDebutExperienceProfessionnel = '$dateDebut' ";
-        }
-        if (strlen($dateFin) > 0){
+        } elseif (strlen($dateFin) > 0) {
             $sql .= "AND dateFinExperienceProfessionnel = '$dateFin' ";
         }
-        if (strlen($codePostal) > 0){
+        if (strlen($codePostal) > 0) {
             $sql .= "AND codePostalExperienceProfessionnel = '$codePostal' ";
         }
         $sql .= "GROUP BY idAlternance, sujet, thematique, taches, codePostal, adresse, dateDebut, dateFin, etudiant, enseignant, tuteurProfessionnel, datepublication ";
-        if(isset($optionTri)){
-            if ($optionTri == "datePublication"){
+        if (isset($optionTri)) {
+            if ($optionTri == "datePublication") {
                 $sql .= "ORDER BY datePublication ASC";
             }
             if ($optionTri == "datePublicationInverse") {
@@ -143,11 +150,35 @@ class AlternanceRepository{
         }
 
         $requete = $pdo->query($sql);
-        var_dump($requete);
         $alternanceTriee = [];
-        foreach ($requete as $result){
+        foreach ($requete as $result) {
             $alternanceTriee[] = self::construireDepuisTableau($result);
         }
         return $alternanceTriee;
+    }
+
+    public static function search(string $keywords): array
+    {
+        $sql = "SELECT idAlternance, sujetExperienceProfessionnel AS sujet, thematiqueExperienceProfessionnel AS thematique, tachesExperienceProfessionnel AS taches,
+                codePostalExperienceProfessionnel AS codePostal, adresseExperienceProfessionnel AS adresse, dateDebutExperienceProfessionnel AS dateDebut,
+                dateFinExperienceProfessionnel AS dateFin, siret, datePublication, numEtudiant AS etudiant, mailEnseignant AS enseignant, mailTuteurProfessionnel AS tuteurProfessionnel
+                FROM ExperienceProfessionnel e
+                JOIN Alternances a ON a.idAlternance = e.idExperienceProfessionnel
+                WHERE numEtudiant IS NULL
+                AND (sujetExperienceProfessionnel LIKE '%$keywords%'
+                OR thematiqueExperienceProfessionnel LIKE '%$keywords%'
+                OR tachesExperienceProfessionnel LIKE '%$keywords%'
+                OR codePostalExperienceProfessionnel LIKE '%$keywords%'
+                OR adresseExperienceProfessionnel LIKE '%$keywords%'
+                OR siret LIKE '%$keywords%')";
+
+        $requestStatement = Model::getPdo()->query($sql);
+
+        $AllAlternance = [];
+        foreach ($requestStatement as $alternanceTab) {
+            $AllAlternance[] = self::construireDepuisTableau($alternanceTab);
+        }
+        return $AllAlternance;
+
     }
 }
