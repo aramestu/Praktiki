1d856773da0a11cf999abe2cd0c7bce7f1ba0004
Soren Starck <soren.starck@etu.umontpellier.fr>
2023-10-28 18:18:58 +0200

    fixed problem model.php

--- src/Model/Repository/Model.php
+++ src/Model/Repository/Model.php
@@ -3,6 +3,7 @@
 namespace App\SAE\Model\Repository;
 
 use App\SAE\Config\Conf;
+
 use PDO;
 
 class Model
@@ -12,46 +13,24 @@ class Model
 
     private function __construct()
     {
-        // Configuration de la première base de données
-        $dbConfig1 = Conf::getDatabaseConfiguration(0);
-
-        // Configuration de la deuxième base de données
-        $dbConfig2 = Conf::getDatabaseConfiguration(1);
-
-        $timeout = 2; // Définir un timeout en secondes (2 secondes dans cet exemple)
-
         try {
-            // Tentative de connexion à la première base de données avec timeout
-            $this->pdo = new PDO(
-                "mysql:host={$dbConfig1['hostname']};port={$dbConfig1['port']};dbname={$dbConfig1['database']}",
-                $dbConfig1['login'],
-                $dbConfig1['password'],
-                array(
-                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-                    PDO::ATTR_TIMEOUT => $timeout
-                )
-            );
-        } catch (PDOException $e) {
-            die('Impossible de se connecter à la base de données 00.');
-            // En cas d'échec, basculez sur la deuxième base de données sans attendre
-            try{
-                $this->pdo = new PDO(
-                            "mysql:host={$dbConfig2['hostname']};port={$dbConfig2['port']};dbname={$dbConfig2['database']}",
-                            $dbConfig2['login'],
-                            $dbConfig2['password'],
-                            array(
-                                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-                                PDO::ATTR_TIMEOUT => $timeout
-                            )
-                        );
-            }
-            catch (PDOException $e) {
-                die('Impossible de se connecter à la base de données 01.');
-            }
-        }
+            $hostname = Conf::getHostname();
+            $databaseName = Conf::getDatabase();
+            $login = Conf::getLogin();
+            $password = Conf::getPassword();
+            $port = Conf::getPort();
+            // Connexion à la base de données
+            // Le dernier argument sert à ce que toutes les chaînes de caractères
+            // en entrée et sortie de MySQL soient dans le codage UTF-8
+            $this->pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$databaseName", $login, $password,
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
-        // Activez le mode d'affichage des erreurs et le lancement d'exception en cas d'erreur
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // On active le mode d'affichage des erreurs et le lancement d'exception en cas d'erreur
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            die('Erreur connexion base de données');
+        }
     }
 
     public static function getPdo(): PDO
@@ -67,4 +46,3 @@ class Model
         return self::$instance;
     }
 }
-?>
