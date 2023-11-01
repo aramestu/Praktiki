99e5d5e99ca8589222faa98a2196f2581c58e280
Thibaut-au <thibaut.audouy@etu.umontpellier.fr>
2023-11-01 23:52:35 +0100

    Retour en arrière, search générique n'était pas une super idée. IM SAD

--- src/Controller/ControllerEtudiant.php
+++ src/Controller/ControllerEtudiant.php
@@ -17,7 +17,7 @@ class ControllerEtudiant extends ControllerGenerique{
         if(isset($_GET['keywords'])){
             $keywords = urldecode($_GET["keywords"]);
         }
-        $listEtudiants = (new EtudiantRepository())->search($keywords);
+        $listEtudiants = (new EtudiantRepository())->getAll();
         self::afficheVue(
             'view.php',
             [
