3bcc60f6fad6be487365eb8e8e4eb8829a142ba8
Norman Francois <norman.francois@etu.umontpellier.fr>
2023-11-10 00:44:27 +0100

    modifications du style du pannel admin et amélioration de la responsivité (principalement ListeEntreprises)

--- src/View/user/adminPanel/entreprise/entrepriseLine.php
+++ src/View/user/adminPanel/entreprise/entrepriseLine.php
@@ -1,10 +1,10 @@
 <div class="container line">
-    <div>
+    <div class="HBox containerEtatNomEntreprise">
         <div class="circle<?=$entreprise->getEstValide() ? " greenColor" : " orangeColor"?>"></div>
         <label class="lineNomEntreprise"><?= htmlspecialchars($entreprise->getNomEntreprise())?></label>
     </div>
-    <label class="lineCodePostalEntreprise">Code Postal : <?= htmlspecialchars($entreprise->getCodePostalEntreprise())?></label>
-    <label class="lineTelephoneEntreprise">Téléphone : <a href="tel:<?= htmlspecialchars($entreprise->getTelephoneEntreprise())?>"><?= htmlspecialchars($entreprise->getTelephoneEntreprise())?></a></label>
-    <label class="lineSiteWebEntreprise"><a href="https://<?= htmlspecialchars($entreprise->getSiteWebEntreprise())?>">Site web</a></label>
+    <label class="lineCodePostalEntreprise"><?= htmlspecialchars($entreprise->getCodePostalEntreprise())?></label>
+    <label class="lineTelephoneEntreprise"><a class="link" href="tel:<?= htmlspecialchars($entreprise->getTelephoneEntreprise())?>"><?= htmlspecialchars($entreprise->getTelephoneEntreprise())?></a></label>
+    <label class="lineSiteWebEntreprise"><a class="link" href="https://<?= htmlspecialchars($entreprise->getSiteWebEntreprise())?>">Site web</a></label>
     <a><button>Consulter</button></a>
 </div>
