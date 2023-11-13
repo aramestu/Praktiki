cac2a765058886a9deea1fffd1e00f2adfe0e423
francoisn <norman.francois@etu.umontpellier.fr>
2023-11-13 23:08:14 +0100

    refactor [Ldap]: modification du fonctionnement du client Maintenant les informations liées à l'utilisateur sont accessible à partir de fonctions

--- src/Lib/Ldap.php
+++ src/Lib/Ldap.php
@@ -7,11 +7,10 @@ use LDAP\Connection;
 class Ldap
 {
     static private string $ldapApiKey = "LdapAPIPassword";
-
+    static private array $userInformations;
     static private string $adresseServeur = "https://webinfo.iutmontp.univ-montp2.fr/~francoisn/LDAP_API.php";
 
-    static private function getHTTPRequest($fonction, $login, $password)
-    {
+    static private function getHTTPRequest($fonction, $login, $password){
         return array('http' => [
             'method' => 'POST',
             'header' => 'Content-Type: application/x-www-form-urlencoded',
@@ -22,17 +21,38 @@ class Ldap
         ]);
     }
 
-    static public function bind($login, $password): ?bool
-    {
+    static public function connection($login, $password): ?bool{
         $reponse = file_get_contents(self::$adresseServeur, false, stream_context_create(self::getHTTPRequest("bind", $login, $password)));
         if ($reponse == "error") {
             return null;
         }
+        if($reponse == true){
+            self::$userInformations = self::getInfos($login, $password);
+        }
         return (bool)$reponse;
     }
 
-    static public function getInfos($login, $password): ?array
-    {
+    static public function getUserMail(): string{
+        return self::$userInformations["mail"][0];
+    }
+
+    static public function getUserLogin(): string{
+        return self::$userInformations["gecos"][0];
+    }
+
+    static public function getUserName(): string{
+        return self::$userInformations["givenname"][0];
+    }
+
+    static public function getUserSurname(): string{
+        return self::$userInformations["sn"][0];
+    }
+
+    static public function getUserAnnee():string{
+        return explode('/',self::$userInformations["homedirectory"][0])[2];
+    }
+
+    private static function getInfos($login, $password): ?array{
         $reponse = file_get_contents(self::$adresseServeur, false, stream_context_create(self::getHTTPRequest("getInfos", $login, $password)));
         if ($reponse == "error") {
             return null;
