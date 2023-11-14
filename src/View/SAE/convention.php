be531ab625d6e13345f13429074db2c3cbd53003
le-clems <clementhamel05@gmail.com>
2023-11-14 21:17:53 +0100

    refactor: front convention

--- src/View/SAE/convention.php
+++ src/View/SAE/convention.php
@@ -3,111 +3,145 @@
 
 <div class="container">
     <h2>Etudiant : </h2>
-    <label for="nomEtudiant">Nom de l'étudiant</label>
-    <input type="text" name="nomEtudiant" id="nomEtudiant" placeholder="nomEtudiant">
-
-    <label for="prenomEtudiant">Prénom de l'étudiant</label>
-    <input type="text" name="prenomEtudiant" id="prenomEtudiant" placeholder="prenomEtudiant">
-
-    <label for="addresseEtudiant">Adresse de l'étudiant</label>
-    <input type="text" name="addresseEtudiant" id="addresseEtudiant" placeholder="addresseEtudiant">
-
-    <label for="telephoneEtudiant">Téléphone de l'étudiant</label>
-    <input type="text" name="telephoneEtudiant" id="telephoneEtudiant" placeholder="telephoneEtudiant">
-
-    <label for="courrielEtudiant">Courriel de l'étudiant</label>
-    <input type="text" name="courrielEtudiant" id="courrielEtudiant" placeholder="courrielEtudiant">
-
-    <label for="assuranceMaladie">Assurance maladie de l'étudiant</label>
-    <input type="text" name="assuranceMaladie" id="assuranceMaladie" placeholder="assuranceMaladie">
+    <div class="container-label-input">
+        <label for="nomEtudiant">Nom de l'étudiant</label>
+        <input type="text" name="nomEtudiant" id="nomEtudiant" placeholder="Nom de l'étudiant">
+    </div>
+    <div class="container-label-input">
+        <label for="prenomEtudiant">Prénom de l'étudiant</label>
+        <input type="text" name="prenomEtudiant" id="prenomEtudiant" placeholder="Prénom de l'étudiant">
+    </div>
+    <div class="container-label-input">
+        <label for="addresseEtudiant">Adresse de l'étudiant</label>
+        <input type="text" name="addresseEtudiant" id="addresseEtudiant" placeholder="Adresse de l'étudiant">
+    </div>
+    <div class="container-label-input">
+        <label for="telephoneEtudiant">Téléphone de l'étudiant</label>
+        <input type="text" name="telephoneEtudiant" id="telephoneEtudiant" placeholder="Téléphone de l'étudiant">
+    </div>
+    <div class="container-label-input">
+        <label for="courrielEtudiant">Courriel de l'étudiant</label>
+        <input type="text" name="courrielEtudiant" id="courrielEtudiant" placeholder="Courriel de l'étudiant">
+    </div>
+    <div class="container-label-input">
+        <label for="assuranceMaladie">Assurance maladie de l'étudiant</label>
+        <input type="text" name="assuranceMaladie" id="assuranceMaladie" placeholder="Assurance maladie de l'étudiant">
+    </div>
 </div>
 
 <div class="container">
     <h2>Stage et entreprise : </h2>
-    <label for="typeStage">Type du stage</label>
-    <input type="text" name="typeStage" id="typeStage" placeholder="typeStage">
-
-    <label for="thematiqueStage">Thématique du stage</label>
-    <input type="text" name="thematiqueStage" id="thematiqueStage" placeholder="thematiqueStage">
-
-    <label for="sujetStage">Sujet du stage</label>
-    <input type="text" name="sujetStage" id="sujetStage" placeholder="sujetStage">
-
-    <label for="fonctionsEtTache">Fonctions et tâches du stage</label>
-    <input type="text" name="fonctionsEtTache" id="fonctionsEtTache" placeholder="fonctionsEtTache">
-
-    <label for="competences">Compétence à développer ou à acquérir</label>
-    <input type="text" name="competences" id="competences" placeholder="competences">
-
-    <p>Période du stage</p>
-    <label for="debutPeriodeStage">Début</label>
-    <input type="date" name="debutPeriodeStage" id="debutPeriodeStage">
-
-    <label for="finPeriodeStage">Fin</label>
-    <input type="date" name="finPeriodeStage" id="finPeriodeStage">
-
-    <label for="langueConvention">Langue de la convention</label>
-    <input type="text" name="langueConvention" id="langueConvention" placeholder="langueConvention">
-
-    <label for="gratificationStage">Gratification lors du stage</label>
-    <input type="number" name="gratificationStage" id="gratificationStage" placeholder="gratificationStage">
-
-    <label for="origineStage">Origine du stage</label>
-    <input type="text" name="origineStage" id="origineStage" placeholder="origineStage">
-
-    <label for="confidentialite">Confidentialité du sujet et du thème du Stage</label>
-    <input type="text" name="confidentialite" id="confidentialite" placeholder="confidentialite">
-
-    <label for="nombreHeureHebdo">Nombre d'heures par semaine</label>
-    <input type="number" name="nombreHeureHebdo" id="nombreHeureHebdo" placeholder="nombreHeureHebdo">
-
-    <label for="modaliteVersement">Modalité du versement</label>
-    <input type="text" name="modaliteVersement" id="modaliteVersement" placeholder="modaliteVersement">
-
-    <label for="travailApresStage">Nature du travail après le stage</label>
-    <input type="text" name="travailApresStage" id="travailApresStage" placeholder="travailApresStage">
-
-    <label for="modaliteValidationStage">Modalité de validation du stage</label>
-    <input type="text" name="modaliteValidationStage" id="modaliteValidationStage" placeholder="modaliteValidationStage">
-
-    <label for="dureeStage">Durée du stage</label>
-    <input type="number" name="dureeStage" id="dureeStage" placeholder="dureeStage">
-
-    <label for="nomEntreprise">Nom de l'entreprise</label>
-    <input type="text" name="nomEntreprise" id="nomEntreprise" placeholder="nomEntreprise">
-
-    <label for="addresseEntreprise">Addresse de l'entreprise</label>
-    <input type="text" name="addresseEntreprise" id="addresseEntreprise" placeholder="addresseEntreprise">
-
-    <label for="nomTuteur">Nom du tuteur</label>
-    <input type="text" name="nomTuteur" id="nomTuteur" placeholder="nomTuteur">
-
-    <label for="prenomTuteur">Prénom du tuteur</label>
-    <input type="text" name="prenomTuteur" id="prenomTuteur" placeholder="prenomTuteur">
-
-    <label for="professionTuteur">Profession du tuteur</label>
-    <input type="text" name="professionTuteur" id="professionTuteur" placeholder="professionTuteur">
-
-    <label for="mailTuteur">Mail du tuteur</label>
-    <input type="text" name="mailTuteur" id="mailTuteur" placeholder="mailTuteur">
-
-    <label for="nomSignataire">Nom du signataire</label>
-    <input type="text" name="nomSignataire" id="nomSignataire" placeholder="nomSignataire">
-
-    <label for="prenomSignataire">Prénom du signataire</label>
-    <input type="text" name="prenomSignataire" id="prenomSignataire" placeholder="prenomSignataire">
-
-    <label for="professionSignataire">Profession du signataire</label>
-    <input type="text" name="professionSignataire" id="professionSignataire" placeholder="professionSignataire">
+    <div class="container-label-input">
+        <label for="typeStage">Type du stage</label>
+        <input type="text" name="typeStage" id="typeStage" placeholder="Type du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="thematiqueStage">Thématique du stage</label>
+        <input type="text" name="thematiqueStage" id="thematiqueStage" placeholder="Thématique du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="sujetStage">Sujet du stage</label>
+        <input type="text" name="sujetStage" id="sujetStage" placeholder="Sujet du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="fonctionsEtTache">Fonctions et tâches du stage</label>
+        <input type="text" name="fonctionsEtTache" id="fonctionsEtTache" placeholder="Fonctions et tâches du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="competences">Compétence à développer ou à acquérir</label>
+        <input type="text" name="competences" id="competences" placeholder="Compétence à développer ou à acquérir">
+    </div>
+    <div class="container-label-input">
+        <label>Période du stage</label>
+        <h for="debutPeriodeStage">Début</h>
+        <input type="date" name="debutPeriodeStage" id="debutPeriodeStage" placeholder="Début">
+        <h for="finPeriodeStage">Fin</h>
+        <input type="date" name="finPeriodeStage" id="finPeriodeStage" placeholder="Fin">
+    </div>
+    <div class="container-label-input">
+        <label for="langueConvention">Langue de la convention</label>
+        <input type="text" name="langueConvention" id="langueConvention" placeholder="Langue de la convention">
+    </div>
+    <div class="container-label-input">
+        <label for="gratificationStage">Gratification lors du stage</label>
+        <input type="number" name="gratificationStage" id="gratificationStage" placeholder="Gratification lors du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="origineStage">Origine du stage</label>
+        <input type="text" name="origineStage" id="origineStage" placeholder="Origine du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="confidentialite">Confidentialité du sujet et du thème du Stage</label>
+        <input type="text" name="confidentialite" id="confidentialite" placeholder="Confidentialité du sujet et du thème du Stage">
+    </div>
+    <div class="container-label-input">
+        <label for="nombreHeureHebdo">Nombre d'heures par semaine</label>
+        <input type="number" name="nombreHeureHebdo" id="nombreHeureHebdo" placeholder="Nombre d'heures par semaine">
+    </div>
+    <div class="container-label-input">
+        <label for="modaliteVersement">Modalité du versement</label>
+        <input type="text" name="modaliteVersement" id="modaliteVersement" placeholder="Modalité du versement">
+    </div>
+    <div class="container-label-input">
+        <label for="travailApresStage">Nature du travail après le stage</label>
+        <input type="text" name="travailApresStage" id="travailApresStage" placeholder="Nature du travail après le stage">
+    </div>
+    <div class="container-label-input">
+        <label for="modaliteValidationStage">Modalité de validation du stage</label>
+        <input type="text" name="modaliteValidationStage" id="modaliteValidationStage" placeholder="Modalité de validation du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="dureeStage">Durée du stage</label>
+        <input type="number" name="dureeStage" id="dureeStage" placeholder="Durée du stage">
+    </div>
+    <div class="container-label-input">
+        <label for="nomEntreprise">Nom de l'entreprise</label>
+        <input type="text" name="nomEntreprise" id="nomEntreprise" placeholder="Nom de l'entreprise">
+    </div>
+    <div class="container-label-input">
+        <label for="addresseEntreprise">Addresse de l'entreprise</label>
+        <input type="text" name="addresseEntreprise" id="addresseEntreprise" placeholder="Addresse de l'entreprise">
+    </div>
+    <div class="container-label-input">
+        <label for="nomTuteur">Nom du tuteur</label>
+        <input type="text" name="nomTuteur" id="nomTuteur" placeholder="Nom du tuteur">
+    </div>
+    <div class="container-label-input">
+        <label for="prenomTuteur">Prénom du tuteur</label>
+        <input type="text" name="prenomTuteur" id="prenomTuteur" placeholder="Prénom du tuteur">
+    </div>
+    <div class="container-label-input">
+        <label for="professionTuteur">Profession du tuteur</label>
+        <input type="text" name="professionTuteur" id="professionTuteur" placeholder="Profession du tuteur">
+    </div>
+    <div class="container-label-input">
+        <label for="mailTuteur">Mail du tuteur</label>
+        <input type="text" name="mailTuteur" id="mailTuteur" placeholder="Mail du tuteur">
+    </div>
+    <div class="container-label-input">
+        <label for="nomSignataire">Nom du signataire</label>
+        <input type="text" name="nomSignataire" id="nomSignataire" placeholder="Nom du signataire">
+    </div>
+    <div class="container-label-input">
+        <label for="prenomSignataire">Prénom du signataire</label>
+        <input type="text" name="prenomSignataire" id="prenomSignataire" placeholder="Prénom du signataire">
+    </div>
+    <div class="container-label-input">
+        <label for="professionSignataire">Profession du signataire</label>
+        <input type="text" name="professionSignataire" id="professionSignataire" placeholder="Profession du signataire">
+    </div>
 </div>
 
 <div class="container">
     <h2>Etablissement : </h2>
-    <label for="nomEnseignant">Nom de l'enseignant référant</label>
-    <input type="text" name="nomEnseignant" id="nomEnseignant" placeholder="nomEnseignant">
-
-    <label for="prenomEnseignant">Prénom de l'enseignant référant</label>
-    <input type="text" name="prenomEnseignant" id="prenomEnseignant" placeholder="prenomEnseignant">
+    <div class="container-label-input">
+        <label for="nomEnseignant">Nom de l'enseignant référant</label>
+        <input type="text" name="nomEnseignant" id="nomEnseignant" placeholder="Nom de l'enseignant référant">
+    </div>
+    <div class="container-label-input">
+        <label for="prenomEnseignant">Prénom de l'enseignant référant</label>
+        <input type="text" name="prenomEnseignant" id="prenomEnseignant" placeholder="Prénom de l'enseignant référant">
+    </div>
 </div>
 
 <div>
