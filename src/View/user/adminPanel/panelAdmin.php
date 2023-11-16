4f66cc4834a626b9fbe45824f9d5290c12fbefde
Soren Starck <soren.starck@etu.umontpellier.fr>
2023-11-16 20:53:03 +0100

    avancement panel admin

--- src/View/user/adminPanel/panelAdmin.php
+++ src/View/user/adminPanel/panelAdmin.php
@@ -1,17 +1,23 @@
 <link rel="stylesheet" href="assets/css/panelAdmin.css">
 <link rel="stylesheet" href="assets/css/button.css">
 
-<p><b>Bienvenue Mr. Trombettoni</b></p>
 <div class="HBox" id="panel">
     <div class="container" id="option">
-        <a href="frontController.php?controller=PanelAdmin&action=PanelListeEtudiants" class="button">Liste des Étudiants</a>
-        <a href="frontController.php?action=panelListeEntreprises&controller=PanelAdmin" class="button">Liste des Entreprise</a>
-        <a href="frontController.php?action=getExpProByDefault&controller=ExpPro" class="button" >Liste des offres</a>
+        <div class="HBox">
+            <img src="assets/images/trombettoni.jpg" id="adminPP" alt="Photo de profil de l'admin">
+            <label id="adminName">Gilles Trombettoni</label>
+            <div id="adminBackground"></div>
+        </div>
+        <a href="frontController.php?controller=PanelAdmin&action=PanelListeEtudiants" class="button">Liste des
+            Étudiants</a>
+        <a href="frontController.php?action=panelListeEntreprises&controller=PanelAdmin" class="button">Liste des
+            Entreprise</a>
+        <a href="frontController.php?action=getExpProByDefault&controller=ExpPro" class="button">Liste des offres</a>
         <a href="frontController.php?action=import" class="button">Importation des données</a>
     </div>
     <div class="container" id="placeholder">
         <?php
-            require __DIR__ . "/../../$adminPanelView";
+        require __DIR__ . "/../../$adminPanelView";
         ?>
     </div>
 </div>
