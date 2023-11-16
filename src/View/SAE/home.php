ef98e4ce006f2ef66630f2ef40fc556ac3dc1545
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2023-11-17 00:21:53 +0100

    Fix : Mise en page home

--- src/View/SAE/home.php
+++ src/View/SAE/home.php
@@ -14,18 +14,19 @@
     <h3>Où trouver des Offres de stage</h3>
     <br>
     <p>Les stages se trouvent dans la partie offre du site, si malgré cela, vous n'en trouvez pas, voici quelque lien pour vous aidez.</p>
-    <p><a href="https://www.welcometothejungle.com/fr/jobs?page=1&amp;groupBy=job&amp;sortBy=mostRelevant&amp;query=informatique&amp;aroundQuery=Montpellier%2C%20France&amp;aroundLatLng=43.61086%2C3.87613&amp;aroundRadius=50000&amp;refinementList%5Bcontract_type_names.fr%5D%5B%5D=Stage">Welcome To The Jungle</a></p>
-    <p><a href="https://www.apec.fr/candidat.html" target="_blank" rel="noopener">APEC</a></p>
-    <p><a href="https://www.pole-emploi.fr/accueil/" target="_blank" rel="noopener">Pole Emploi</a></p>
-    <p><a href="https://fr.linkedin.com/" target="_blank" rel="noopener">LinkedIn</a></p>
-    <p><a href="https://www.1jeune1solution.gouv.fr/stages">https://www.1jeune1solution.gouv.fr/stages</a></p>
-    <p><a href="https://jobs-stages.letudiant.fr/stages-etudiants.html">L&#8217;Etudiant</a></p>
+    <br>
+    <p><a href="https://www.welcometothejungle.com/fr/jobs?page=1&amp;groupBy=job&amp;sortBy=mostRelevant&amp;query=informatique&amp;aroundQuery=Montpellier%2C%20France&amp;aroundLatLng=43.61086%2C3.87613&amp;aroundRadius=50000&amp;refinementList%5Bcontract_type_names.fr%5D%5B%5D=Stage" class="link">Welcome To The Jungle</a></p>
+    <p><a href="https://www.apec.fr/candidat.html" target="_blank" rel="noopener" class="link">APEC</a></p>
+    <p><a href="https://www.pole-emploi.fr/accueil/" target="_blank" rel="noopener" class="link">Pole Emploi</a></p>
+    <p><a href="https://fr.linkedin.com/" target="_blank" rel="noopener" class="link">LinkedIn</a></p>
+    <p><a href="https://www.1jeune1solution.gouv.fr/stages" class="link">https://www.1jeune1solution.gouv.fr/stages</a></p>
+    <p><a href="https://jobs-stages.letudiant.fr/stages-etudiants.html" class="link">L&#8217;Etudiant</a></p>
     <br>
     <h3>Candidatures spontanées:</h3>
     <br>
     <p>Vous pouvez vous déplacer dans les incubateurs d&#8217;entreprises (Cap Omega, Cap Alpha&#8230;)</p>
     <p>Entreprises dans lesquels sont d&#8217;anciens étudiants du dept&#8230; (regardez LinkedIn)</p>
-    <p>Consultez les consignes de communication pour vos CV/candidatures/entretiens voir <a href="https://moodle.umontpellier.fr/course/view.php?id=278">Cours Com Moodle</a></p>
+    <p>Consultez les consignes de communication pour vos CV/candidatures/entretiens voir <a href="https://moodle.umontpellier.fr/course/view.php?id=278" class="link">Cours Com Moodle</a></p>
     <p>Faites relire votre CV</p>
     <p>Personnalisez votre lettre de motivation (un texte générique ou copié/collé avec le nom d&#8217;une autre entreprise est souvent rédhibitoire)</p>
     <p>Utilisez une adresse email incluant votre nom</p>
@@ -36,7 +37,7 @@
     <br>
     <p>La gratification est <b>obligatoire</b> au delà de 2 mois de stage (soit l&#8217;équivalent de 44 jours à 7 heures par jour ou soit à partir de la 309e heure de stage même s&#8217;il est effectué de façon non continue).</p>
     <p>Le <b>montant minimum</b> légal de gratification est de<b> 4,35 euros*</b> par heure de stage.  *( modification 1er Janvier 2024)</p>
-    <p>Vous trouverez des informations supplémentaires sur vos droits et devoirs en tant que stagiaire <a href="https://www.service-public.fr/particuliers/vosdroits/F16734">ici</a>, ainsi qu&#8217;un simulateur de votre rémunération en cliquant <a href="https://www.service-public.fr/particuliers/vosdroits/F32131#:~:text=Montant%20minimum,%E2%82%AC%20x%200%2C15)." target="_blank" rel="noopener noreferrer">ici</a>.</p>
+    <p>Vous trouverez des informations supplémentaires sur vos droits et devoirs en tant que stagiaire <a href="https://www.service-public.fr/particuliers/vosdroits/F16734" class="link">ici</a>, ainsi qu&#8217;un simulateur de votre rémunération en cliquant <a href="https://www.service-public.fr/particuliers/vosdroits/F32131#:~:text=Montant%20minimum,%E2%82%AC%20x%200%2C15)." target="_blank" rel="noopener noreferrer">ici</a>.</p>
 </div>
 
 <div class="container">
@@ -52,18 +53,16 @@
     <br>
     <h3>Documents utiles</h3>
     <br>
-    <a href="https://drive.google.com/drive/folders/1C6qphFXaaBHH4DKaqqywgEstMovD3xNt">Les calendriers de l&#8217;alternance en BUT2 et BUT3</a><br>
-    <a href="https://drive.google.com/file/d/1wphXCq6cAdcwhwTAiDW8v20xWQOAEDAJ/view?usp=share_link">Le livret de l&#8217;alternant</a><br>
-    <a href="https://drive.google.com/file/d/1oUkhEWFKWemTAbnEX7fRGmZN23C3Ve1F/view?usp=sharing">Le programme nationnal BUT Info (tous parcours confondus)</a><br>
+    <a href="https://drive.google.com/drive/folders/1C6qphFXaaBHH4DKaqqywgEstMovD3xNt" class="link">Les calendriers de l&#8217;alternance en BUT2 et BUT3</a><br>
+    <a href="https://drive.google.com/file/d/1wphXCq6cAdcwhwTAiDW8v20xWQOAEDAJ/view?usp=share_link" class="link">Le livret de l&#8217;alternant</a><br>
+    <a href="https://drive.google.com/file/d/1oUkhEWFKWemTAbnEX7fRGmZN23C3Ve1F/view?usp=sharing" class="link">Le programme nationnal BUT Info (tous parcours confondus)</a><br>
     <br>
     <h3>Rémunération</h3>
     <br>
     <p>La rémunération minimale réglementaire d’un apprenti·e est basée sur trois critères :</p>
-    <ul>
-        <li>Le critère principal est celui de l’<strong>année contractuelle</strong> &#8211; année d’exécution du contrat ;</li>
-        <li>La <strong>tranche d’âge de l’apprenti au moment de l’embauche, et son évolution</strong> dans le temps, le cas échéant ;</li>
-        <li>Son <strong>évolution dans le cycle de formation suivie</strong> : en principe, d’une durée désormais de 6 mois à 3 ans maximum.</li>
-    </ul>
+    <p>Le critère principal est celui de l’<strong>année contractuelle</strong> &#8211; année d’exécution du contrat ;</p>
+    <p>La <strong>tranche d’âge de l’apprenti au moment de l’embauche, et son évolution</strong> dans le temps, le cas échéant ;</p>
+    <p>Son <strong>évolution dans le cycle de formation suivie</strong> : en principe, d’une durée désormais de 6 mois à 3 ans maximum.</p>
     <p>La rémunération dépend des dispositions conventionnelles de l’entreprise, à défaut, c’est le salaire minimum de croissance en vigueur (SMIC).</p>
     <br>
     <table id="tableRemuneration">
