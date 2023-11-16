99a3f3ebfe27622f859b56e1ded77299d8c94b42
francoisn <norman.francois@etu.umontpellier.fr>
2023-11-16 01:43:44 +0100

    refactor [Panel Admin]: suppressions des boutons d'options inutile dans la page principale

--- src/View/user/adminPanel/panelAdmin.php
+++ src/View/user/adminPanel/panelAdmin.php
@@ -4,11 +4,9 @@
 <p><b>Bienvenue Mr. Trombettoni</b></p>
 <div class="HBox" id="panel">
     <div class="container" id="option">
-        <a href="frontController.php?controller=PanelAdmin&action=PanelListeEtudiants" class="button">Liste des étudiants</a>
-        <a href="" class="button" >Liste des offres en Attentes</a>
-        <a href="frontController.php?action=panelEntreprises&controller=PanelAdmin" class="button">Liste des entreprises en Attentes</a>
-        <a href="frontController.php?action=getExpProByDefault&controller=ExpPro" class="button" >Liste des offres</a>
+        <a href="frontController.php?controller=PanelAdmin&action=PanelListeEtudiants" class="button">Liste des Étudiants</a>
         <a href="frontController.php?action=panelListeEntreprises&controller=PanelAdmin" class="button">Liste des Entreprise</a>
+        <a href="frontController.php?action=getExpProByDefault&controller=ExpPro" class="button" >Liste des offres</a>
         <a href="frontController.php?action=import" class="button">Importation des données</a>
     </div>
     <div class="container" id="placeholder">
