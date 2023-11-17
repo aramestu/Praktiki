8df5a51971f6ecb156bff4b0f02a0db8367dc41f
Soren Starck <soren.starck@etu.umontpellier.fr>
2023-11-17 01:05:20 +0100

    liste de bouton panel admin

--- src/View/user/adminPanel/panelAdmin.php
+++ src/View/user/adminPanel/panelAdmin.php
@@ -8,12 +8,10 @@
             <label id="adminName">Gilles Trombettoni</label>
             <div id="adminBackground"></div>
         </div>
-        <a href="frontController.php?controller=PanelAdmin&action=PanelListeEtudiants" class="button">Liste des
-            Étudiants</a>
-        <a href="frontController.php?action=panelListeEntreprises&controller=PanelAdmin" class="button">Liste des
-            Entreprise</a>
-        <a href="frontController.php?action=getExpProByDefault&controller=ExpPro" class="button">Liste des offres</a>
-        <a href="frontController.php?action=import" class="button">Importation des données</a>
+        <a href="frontController.php?controller=PanelAdmin&action=PanelListeEtudiants" class="button"><div class="HBox"><img src="assets/images/etudiant-icon.png">Liste des Étudiants</div></a>
+        <a href="frontController.php?action=panelListeEntreprises&controller=PanelAdmin" class="button"><div class="HBox"><img src="assets/images/company-icon.png">Liste des Entreprise</div></a>
+        <a href="frontController.php?action=getExpProByDefault&controller=ExpPro" class="button"><div class="HBox"><img src="assets/images/offre-icon.png">Liste des offres</div></a>
+        <a href="frontController.php?action=import" class="button"><div class="HBox"><img src="assets/images/upload-icon.png">Importation des données</div></a>
     </div>
     <div class="container" id="placeholder">
         <?php
