681464abfed5300b61f7e87f3be6e780401290ff
francoisn <norman.francois@etu.umontpellier.fr>
2023-11-23 00:45:15 +0100

    refactor [LDAP]: refactoring des connexions utilisateurs afin de les rendre plus lisible + connection du brutforce enseignant au front

--- src/View/user/preference.php
+++ src/View/user/preference.php
@@ -3,22 +3,22 @@
     <form method="get">
         <h2 id="remplaceBaliseLegend">Choisissez votre compte</h2>
         <p>
-            <input type="hidden" name="action" value="connect">
-            <input type="hidden" name="controller" value="LDAP">
+            <input type="hidden" name="action" value="afficherConnexionLdap">
+            <input type="hidden" name="controller" value="Connexion">
             <input type="submit" value="Compte IUT">
     </form>
     <form method="get">
         <p>
-            <input type="hidden" name="action" value="connect">
-            <input type="hidden" name="controller" value="Entreprise">
+            <input type="hidden" name="action" value="afficherConnexionEntreprise">
+            <input type="hidden" name="controller" value="Connexion">
             <input type="submit" value="Compte Entreprise">
         </p>
     </form>
     <form method="get">
         <p>
-            <input type="hidden" name="action" value="connect">
-            <input type="hidden" name="controller" value="Enseignant">
-            <input type="submit" value="Compte Enseignant">
+            <input type="hidden" name="action" value="afficherConnexionPersonnel">
+            <input type="hidden" name="controller" value="Connexion">
+            <input type="submit" value="Compte Personnel">
         </p>
     </form>
 </div>
