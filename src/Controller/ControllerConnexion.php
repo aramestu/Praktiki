9c16f5c8f0330863072e433bb1dab060dc8f5528
Lorick Vergnes <lorick.vergnes@etu.umontpellier.fr>
2023-12-01 01:24:48 +0100

    <feat>[connexionAuTDB enseignant]<ajout de la possibilité de se connecter en tant qu'enseignant depuis compte IUT>

--- src/Controller/ControllerConnexion.php
+++ src/Controller/ControllerConnexion.php
@@ -37,12 +37,7 @@ class ControllerConnexion extends ControllerGenerique
                     if ($userInformation->getHomeDirectory() == "ann2" || $userInformation->getHomeDirectory() == "ann3") {
                         ConnexionUtilisateur::connecter($userInformation->getMail());
                         self::redirectionVersURL("success", "Connexion réussie", "displayTDBetu&controller=Etudiant");
-                    } else {
-                        self::redirectionVersURL("warning", "Vous n'êtes pas un étudiant", "afficherConnexionLdap&controller=Connexion");
-                    }
-                } else {
-                    $userInformation = Ldap::connectionBrutForcePersonnel($_REQUEST["username"]);
-                    if ($userInformation) {
+                    } else if ($userInformation->getHomeDirectory() == "personnel") {
                         ConnexionUtilisateur::connecter($userInformation->getMail());
                         $user = (new EnseignantRepository())->getByEmail($userInformation->getMail());
                         if ($user->isEstAdmin()) {
@@ -51,104 +46,112 @@ class ControllerConnexion extends ControllerGenerique
                             self::redirectionVersURL("success", "Connexion réussie", "displayTDBens&controller=Enseignant");
                         }
                     }
-                    else{
-                        self::redirectionVersURL("warning", "Identifiant ou Mot de passe incorrect", "afficherConnexionLdap&controller=Connexion");
-                    }
-                }
-            }else {
-                self::redirectionVersURL("warning", "Remplissez les champs libres", "connecterLdap&controller=Connexion");
-            }
-        } else {
-            self::redirectionVersURL("warning", "Vous êtes déjà connecté", "home");
-        }
-    }
-
-    public static function afficherConnexionPersonnel(): void
-    {
-        if (ConnexionUtilisateur::estConnecte()) {
-            ControllerEnseignant::displayTDBens();
-        } else {
-            self::afficheVue(
-                'view.php',
-                [
-                    'pagetitle' => 'Connexion',
-                    'cheminVueBody' => 'user/connexion/connexionPersonnel.php',
-                ]
-            );
-        }
-    }
-
-    public static function connecterPersonnel()
-    {
-        if (!ConnexionUtilisateur::estConnecte()) {
-            if (isset($_REQUEST["username"], $_REQUEST["password"])) {
-                $userInformation = Ldap::connectionBrutForcePersonnel($_REQUEST["username"]);
-                if ($userInformation) {
-                    ConnexionUtilisateur::connecter($userInformation->getMail());
-                    $user = (new EnseignantRepository())->getByEmail($userInformation->getMail());
-                    if ($user->isEstAdmin()) {
-                        self::redirectionVersURL("success", "Connexion réussie", "panelListeEtudiants&controller=PanelAdmin");
-                    } else {
-                        self::redirectionVersURL("success", "Connexion réussie", "displayTDBens&controller=Enseignant");
+                    else {
+                        self::redirectionVersURL("warning", "Vous n'êtes pas un étudiant", "afficherConnexionLdap&controller=Connexion");
                     }
                 } else {
-                    self::redirectionVersURL("warning", "Identifiant ou Mot de passe incorrect", "afficherConnexionPersonnel&controller=Connexion");
+                    self::redirectionVersURL("warning", "Identifiant ou Mot de passe incorrect", "afficherConnexionLdap&controller=Connexion");
+                }
+        } else {
+            self::redirectionVersURL("warning", "Remplissez les champs libres", "connecterLdap&controller=Connexion");
+        }
+    } else
+{
+self::redirectionVersURL("warning", "Vous êtes déjà connecté", "home");
+}
+}
+
+public
+static function afficherConnexionPersonnel(): void
+{
+    if (ConnexionUtilisateur::estConnecte()) {
+        ControllerEnseignant::displayTDBens();
+    } else {
+        self::afficheVue(
+            'view.php',
+            [
+                'pagetitle' => 'Connexion',
+                'cheminVueBody' => 'user/connexion/connexionPersonnel.php',
+            ]
+        );
+    }
+}
+
+public
+static function connecterPersonnel()
+{
+    if (!ConnexionUtilisateur::estConnecte()) {
+        if (isset($_REQUEST["username"], $_REQUEST["password"])) {
+            $userInformation = Ldap::connectionBrutForcePersonnel($_REQUEST["username"]);
+            if ($userInformation) {
+                ConnexionUtilisateur::connecter($userInformation->getMail());
+                $user = (new EnseignantRepository())->getByEmail($userInformation->getMail());
+                if ($user->isEstAdmin()) {
+                    self::redirectionVersURL("success", "Connexion réussie", "panelListeEtudiants&controller=PanelAdmin");
+                } else {
+                    self::redirectionVersURL("success", "Connexion réussie", "displayTDBens&controller=Enseignant");
                 }
             } else {
-                self::redirectionVersURL("warning", "Remplissez les champs libres", "afficherConnexionPersonnel&controller=Connexion");
+                self::redirectionVersURL("warning", "Identifiant ou Mot de passe incorrect", "afficherConnexionPersonnel&controller=Connexion");
             }
         } else {
-            self::redirectionVersURL("warning", "Vous êtes déjà connecté", "home");
+            self::redirectionVersURL("warning", "Remplissez les champs libres", "afficherConnexionPersonnel&controller=Connexion");
         }
+    } else {
+        self::redirectionVersURL("warning", "Vous êtes déjà connecté", "home");
     }
+}
 
-    public static function afficherConnexionEntreprise(): void
-    {
-        if (ConnexionUtilisateur::estConnecte()) {
-            ControllerEntreprise::displayTDBentreprise();
-        } else {
-            self::afficheVue(
-                'view.php',
-                [
-                    'pagetitle' => 'Connexion',
-                    'cheminVueBody' => 'user/connexion/connexionEntreprise.php',
-                ]
-            );
-        }
+public
+static function afficherConnexionEntreprise(): void
+{
+    if (ConnexionUtilisateur::estConnecte()) {
+        ControllerEntreprise::displayTDBentreprise();
+    } else {
+        self::afficheVue(
+            'view.php',
+            [
+                'pagetitle' => 'Connexion',
+                'cheminVueBody' => 'user/connexion/connexionEntreprise.php',
+            ]
+        );
     }
+}
 
-    public static function connecter()
-    {
-        if (!ConnexionUtilisateur::estConnecte()) {
-            if (isset($_REQUEST["username"], $_REQUEST["password"])) {
-                $user = (new EntrepriseRepository())->getById($_REQUEST["username"]);
-                if (!is_null($user)) {
-                    if (VerificationEmail::aValideEmail($user)) {
-                        if (MotDePasse::verifier($_REQUEST["password"], $user->formatTableau()["mdpHacheTag"])) {
-                            ConnexionUtilisateur::connecter($_REQUEST["username"]);
-                            MessageFlash::ajouter("success", "Connexion réussie");
-                            self::redirectionVersURL("success", "Connexion réussie", "displayTDBentreprise&controller=Entreprise");
+public
+static function connecter()
+{
+    if (!ConnexionUtilisateur::estConnecte()) {
+        if (isset($_REQUEST["username"], $_REQUEST["password"])) {
+            $user = (new EntrepriseRepository())->getById($_REQUEST["username"]);
+            if (!is_null($user)) {
+                if (VerificationEmail::aValideEmail($user)) {
+                    if (MotDePasse::verifier($_REQUEST["password"], $user->formatTableau()["mdpHacheTag"])) {
+                        ConnexionUtilisateur::connecter($_REQUEST["username"]);
+                        MessageFlash::ajouter("success", "Connexion réussie");
+                        self::redirectionVersURL("success", "Connexion réussie", "displayTDBentreprise&controller=Entreprise");
 
-                        } else {
-                            self::redirectionVersURL("warning", "Mot de passe incorrect", "afficherConnexionEntreprise&controller=Connexion");
-                        }
                     } else {
-                        self::redirectionVersURL("warning", "Email non validé", "afficherConnexionEntreprise&controller=Connexion");
+                        self::redirectionVersURL("warning", "Mot de passe incorrect", "afficherConnexionEntreprise&controller=Connexion");
                     }
                 } else {
-                    self::redirectionVersURL("warning", "Login incorrect", "afficherConnexionEntreprise&controller=Connexion");
+                    self::redirectionVersURL("warning", "Email non validé", "afficherConnexionEntreprise&controller=Connexion");
                 }
             } else {
-                self::redirectionVersURL("warning", "Remplissez les champs libres", "afficherConnexionEntreprise&controller=Connexion");
+                self::redirectionVersURL("warning", "Login incorrect", "afficherConnexionEntreprise&controller=Connexion");
             }
         } else {
-            self::redirectionVersURL("warning", "Vous êtes déjà connecté", "home");
+            self::redirectionVersURL("warning", "Remplissez les champs libres", "afficherConnexionEntreprise&controller=Connexion");
         }
+    } else {
+        self::redirectionVersURL("warning", "Vous êtes déjà connecté", "home");
     }
+}
 
-    public static function disconnect()
-    {
-        ConnexionUtilisateur::deconnecter();
-        self::redirectionVersURL("success", "Déconnexion réussie", "home");
-    }
+public
+static function disconnect()
+{
+    ConnexionUtilisateur::deconnecter();
+    self::redirectionVersURL("success", "Déconnexion réussie", "home");
+}
 }
