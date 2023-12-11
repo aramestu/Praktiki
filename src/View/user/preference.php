1511558e1d6dfabd30fbd9506b240ee5872e21f2
Soren Starck <soren.starck@etu.umontpellier.fr>
2023-12-12 00:50:19 +0100

    refractor page choix compte

--- src/View/user/preference.php
+++ src/View/user/preference.php
@@ -1,7 +1,10 @@
-<link rel="stylesheet" href="assets/css/connect.css">
+<link rel="stylesheet" href="assets/css/connectBIS.css">
+<link rel="stylesheet" href="assets/css/choiceConnection.css">
 <div class="container">
-        <h2 id="remplaceBaliseLegend">Choisissez votre compte</h2>
-        <a class="button" href="frontController.php?action=afficherConnexionLdap&controller=Connexion">Compte IUT</a>
-        <a class="button" href="frontController.php?action=afficherConnexionEntreprise&controller=Connexion">Compte Entreprise</a>
-        <a class="button" href="frontController.php?action=afficherConnexionPersonnel&controller=Connexion">Compte Personnel</a>
+    <h2 id="remplaceBaliseLegend">Choisissez votre compte</h2>
+    <div class="HBox">
+        <a id="studentChoice" class="button" href="frontController.php?action=afficherConnexionLdap&controller=Connexion">Compte IUT</a>
+        <a id="companyChoice" class="button" href="frontController.php?action=afficherConnexionEntreprise&controller=Connexion">Compte Entreprise</a>
+        <a id="professorChoice" class="button" href="frontController.php?action=afficherConnexionPersonnel&controller=Connexion">Compte Personnel</a>
+    </div>
 </div>
