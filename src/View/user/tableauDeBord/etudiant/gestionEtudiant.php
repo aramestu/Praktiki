b81e2825f0c0bc61f52342c74e5fa728fc6aad03
francoisn <norman.francois@etu.umontpellier.fr>
2023-12-12 21:36:21 +0100

    feature [TDB] : ajout des changement de nouveau tableau de bord

--- src/View/user/tableauDeBord/etudiant/gestionEtudiant.php
+++ src/View/user/tableauDeBord/etudiant/gestionEtudiant.php
@@ -4,6 +4,6 @@
         <p>C'est ici que vous pouvez avoir accès aux différentes fonctionnalités du site vous aidant à faire le lien avec votre
             administration </p>
         <a class="button">Créer ma convention de stage</a>
-        <a class="button">Gérer ma convention de stage</a>
+        <a href="frontController.php?controller=Convention&action=afficherFormulaire&idEtudiant=<?php echo $user->getNumEtudiant();?>" class="button">Editer ma Convention</a>
     </div>
 </div>
