a211c632bf331302d0cefa573df17de6cfc068b7
le-clems <clementhamel05@gmail.com>
2024-01-04 13:46:54 +0100

    fix: style des conventions amélioré et textes plus clairs

--- src/View/user/tableauDeBord/etudiant/gestionEtudiant.php
+++ src/View/user/tableauDeBord/etudiant/gestionEtudiant.php
@@ -2,14 +2,14 @@
 <h1>Gestion</h1>
 <div class="VBox">
     <div>
-        <p>C'est ici que vous pouvez avoir accès aux différentes fonctionnalités du site vous aidant à faire le lien avec votre
-            administration </p>
+        <h3 class="bold">C'est ici que vous pouvez avoir accès aux différentes fonctionnalités du site vous aidant à faire le lien avec votre
+            administration.</h3>
         <?php if ($convention == null) {echo '<a href="frontController.php?controller=Convention&action=creerFormulaire&idEtudiant=' . $user->getNumEtudiant() . '" class="button">Créer ma convention de stage</a>';}?>
         <?php
         if ($convention != null) {
             echo '<a href="frontController.php?controller=Convention&action=afficherFormulaire&idEtudiant=' . $user->getNumEtudiant() . '" class="button">Editer ma Convention</a>';
             if (!$convention->getEstFini()) {
-                echo '<a id="confirmationButtonOrigin" class="button">Valider la convention</a>';
+                echo '<a id="confirmationButtonOrigin" class="button">Soumettre ma convention à l\'administration</a>';
             }
             echo "<br>";
             if ($convention->getEstFini()) {
