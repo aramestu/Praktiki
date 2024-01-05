faa4aad7a5803022dfa21d413e2d186b34bdb618
francoisn <norman.francois@etu.umontpellier.fr>
2024-01-05 19:52:08 +0100

    feat [Importation] : ajout de la librairie d'importation ainsi que de la focntion d'import Pstage

---
+++ src/Lib/ImportationData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\SAE\Lib;
+
+use App\SAE\Model\Repository\AnneeUniversitaireRepository;
+use App\SAE\Model\Repository\ConventionRepository;
+use App\SAE\Service\ServiceConvention;
+
+class ImportationData {
+
+    public static function importFromPstage($fileName) {
+        $file = fopen($fileName, "r");
+        $anneeUniversitaireCourante = (new AnneeUniversitaireRepository())->getCurrentAnneeUniversitaire();
+        $isFirstLine = true;
+        while (($column = fgetcsv($file, 10000, ",")) !== false) {
+            if ($isFirstLine) {
+                $isFirstLine = false;
+                continue;
+            }
+            $convention = (new ConventionRepository())->getConventionAvecEtudiant($column[1], $anneeUniversitaireCourante->getIdAnneeUniversitaire());
+            if($convention == null){
+                (new ConventionRepository())->creerConvention($column[1], $anneeUniversitaireCourante->getIdAnneeUniversitaire()); //création de la convention
+                $convention = (new ConventionRepository())->getConventionAvecEtudiant($column[1], $anneeUniversitaireCourante->getIdAnneeUniversitaire());
+            }
+            $attributs = [
+                "estValidee" => $column[28] == "Oui"
+            ];
+
+            (new ServiceConvention())->mettreAJour($convention, $attributs);
+        }
+    }
+}
