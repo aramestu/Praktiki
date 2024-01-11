af62580a79511cd423cb0e213c9faa79e71660af
Soren Starck <soren.starck@etu.umontpellier.fr>
2024-01-11 19:06:20 +0100

    feat : cookie, correction en cours

--- src/Controller/ControllerMain.php
+++ src/Controller/ControllerMain.php
@@ -106,6 +106,29 @@ class ControllerMain extends ControllerGenerique
         );
     }
 
+    public static function setCookie(): void
+    {
+        Cookie::enregistrer('bannerClosed', true, time() + (10 * 365 * 24 * 60 * 60));
+        header('Location: frontController.php?action=home'); // Redirect to home page after setting the cookie
+        exit();
+    }
+
+    public static function checkCookie(): void
+    {
+        if (!Cookie::contient('bannerClosed')) {
+            echo '<div id="cookie-banner"><h2>Politique de confidentialité</h2>
+        <p>Nous utilisons des cookies pour améliorer votre expérience sur notre site. Les cookies sont de petits
+            fichiers de données qui sont stockés sur votre ordinateur ou appareil mobile lorsque vous visitez un site
+            web. Ils nous permettent de collecter des informations sur votre comportement de navigation, comme les pages
+            que vous visitez et les services que vous utilisez. Nous utilisons ces informations pour personnaliser votre
+            expérience, pour comprendre comment notre site est utilisé et pour améliorer nos services. En continuant à
+            utiliser notre site, vous acceptez notre utilisation des cookies. Pour plus dinformations sur notre
+            utilisation des cookies et sur la manière dont vous pouvez contrôler les cookies, veuillez consulter notre
+            politique de confidentialité.</p>
+        <a href="frontController.php?action=setCookie" id="close-banner">Close</a></div>';
+        }
+    }
+
     public static function importation(): void
     {
         if (isset($_POST["import"])) {
