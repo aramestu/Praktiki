81195e71fec932327f47867becb37d1d8a713f4b
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2024-01-11 23:25:05 +0100

    docs: commentaire package Service

--- src/Service/ServiceEnseignant.php
+++ src/Service/ServiceEnseignant.php
@@ -6,7 +6,16 @@ use App\SAE\Model\DataObject\AbstractDataObject;
 use App\SAE\Model\DataObject\Enseignant;
 use App\SAE\Model\Repository\EnseignantRepository;
 
+/**
+ * Service pour l'objet de données Enseignant.
+ */
 class ServiceEnseignant extends AbstractService {
+
+    /**
+     * Obtient le nom du repository correspondant à Enseignant.
+     *
+     * @return string
+     */
     public function getRepository(): string {
         return "EnseignantRepository";
     }
--- src/Service/ServiceEntreprise.php
+++ src/Service/ServiceEntreprise.php
@@ -6,7 +6,16 @@ use App\SAE\Model\DataObject\Entreprise;
 use App\SAE\Model\DataObject\AbstractDataObject;
 use App\SAE\Model\Repository\EntrepriseRepository;
 
+/**
+ * Service pour l'objet de données Entreprise.
+ */
 class ServiceEntreprise extends AbstractService {
+
+    /**
+     * Obtient le nom du repository correspondant à Entreprise.
+     *
+     * @return string
+     */
     public function getRepository(): string {
         return "EntrepriseRepository";
     }
