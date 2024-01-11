1336f576dca226b7152909297ddb28755f32149f
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2024-01-11 20:55:07 +0100

    feat: commentaire packgae Lib

--- src/Lib/ConnexionUtilisateur.php
+++ src/Lib/ConnexionUtilisateur.php
@@ -7,30 +7,52 @@ use App\SAE\Model\Repository\EnseignantRepository;
 use App\SAE\Model\Repository\EntrepriseRepository;
 use App\SAE\Model\Repository\EtudiantRepository;
 
+/**
+ * Classe ConnexionUtilisateur gérant les fonctionnalités de connexion, déconnexion et vérification du statut utilisateur.
+ */
 class ConnexionUtilisateur {
-    // L'Utilisateur connecté sera enregistré en session associé à la clé suivante
+
+    /**
+     * @var string La clé associée à l'utilisateur connecté en session.
+     */
     private static string $cleConnexion = "_utilisateurConnecte";
 
+    /**
+     * Connecte un utilisateur en enregistrant son login en session.
+     *
+     * @param string $loginUtilisateur Le login de l'utilisateur à connecter.
+     */
     public static function connecter(string $loginUtilisateur): void
     {
         $session = Session::getInstance();
         $session->enregistrer(self::$cleConnexion, $loginUtilisateur);
     }
 
+    /**
+     * Vérifie si un utilisateur est connecté.
+     *
+     * @return bool True si un utilisateur est connecté, false sinon.
+     */
     public static function estConnecte(): bool
     {
-        // À compléter
         $session = Session::getInstance();
         return $session->contient(self::$cleConnexion);
     }
 
+    /**
+     * Déconnecte l'utilisateur en supprimant son login de la session.
+     */
     public static function deconnecter(): void
     {
-        // À compléter
         $session = Session::getInstance();
         $session->supprimer(self::$cleConnexion);
     }
 
+    /**
+     * Obtient le login de l'utilisateur connecté.
+     *
+     * @return string|null Le login de l'utilisateur connecté, ou null s'il n'est pas connecté.
+     */
     public static function getLoginUtilisateurConnecte(): ?string
     {
         if (self::estConnecte()) {
@@ -40,11 +62,22 @@ class ConnexionUtilisateur {
         return null;
     }
 
-    public static function estUtilisateur($login): bool
+    /**
+     * Vérifie si l'utilisateur connecté est celui spécifié.
+     *
+     * @param string $login Le login à vérifier.
+     * @return bool True si l'utilisateur connecté correspond au login spécifié, false sinon.
+     */
+    public static function estUtilisateur(string $login): bool
     {
         return (self::getLoginUtilisateurConnecte() == $login);
     }
 
+    /**
+     * Vérifie si l'utilisateur connecté est un étudiant.
+     *
+     * @return bool True si l'utilisateur connecté est un étudiant, false sinon.
+     */
     public static function estEtudiant(): bool
     {
         if (self::estConnecte())
@@ -52,6 +85,11 @@ class ConnexionUtilisateur {
         return false;
     }
 
+    /**
+     * Vérifie si l'utilisateur connecté est une entreprise.
+     *
+     * @return bool True si l'utilisateur connecté est une entreprise, false sinon.
+     */
     public static function estEntreprise(): bool
     {
         if (self::estConnecte())
@@ -59,6 +97,11 @@ class ConnexionUtilisateur {
         return false;
     }
 
+    /**
+     * Vérifie si l'utilisateur connecté est un enseignant.
+     *
+     * @return bool True si l'utilisateur connecté est un enseignant, false sinon.
+     */
     public static function estEnseignant(): bool
     {
         if (self::estConnecte())
@@ -66,6 +109,11 @@ class ConnexionUtilisateur {
         return false;
     }
 
+    /**
+     * Vérifie si l'utilisateur connecté est un administrateur.
+     *
+     * @return bool True si l'utilisateur connecté est un administrateur, false sinon.
+     */
     public static function estAdministrateur(): bool
     {
         if (self::estEnseignant()) {
@@ -74,6 +122,4 @@ class ConnexionUtilisateur {
             return false;
         }
     }
-
-
 }
--- src/Lib/ConversionMajuscule.php
+++ src/Lib/ConversionMajuscule.php
@@ -2,16 +2,29 @@
 
 namespace App\SAE\Lib;
 
+/**
+ * Classe ConversionMajuscule gérant la conversion des caractères d'une chaîne en majuscules avec prise en compte des accents.
+ */
 class ConversionMajuscule {
-    public static function convertirEnMajuscules($string) {
+
+    /**
+     * Convertit une chaîne en majuscules en prenant en compte les accents.
+     *
+     * @param string $string La chaîne à convertir.
+     * @return string La chaîne convertie en majuscules avec gestion des accents.
+     */
+    public static function convertirEnMajuscules(string $string): string
+    {
+        // Tableau de conversion des caractères avec accents
         $conversion = array(
             'à' => 'À', 'â' => 'Â', 'ä' => 'Ä', 'á' => 'Á', 'å' => 'Å',
             'ç' => 'Ç'
         );
 
+        // Conversion en majuscules
         $string = mb_strtoupper($string, 'UTF-8');
-        $string = strtr($string, $conversion);
 
-        return $string;
+        // Remplacement des caractères avec accents
+        return strtr($string, $conversion);
     }
 }
