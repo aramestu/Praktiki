f31acc4ef74ced80ff9ef114715a51b391a7b724
Soren Starck <soren.starck@etu.umontpellier.fr>
2024-01-13 19:16:33 +0100

    feat: ajout icone et titre statistique

--- src/View/user/adminPanel/statistique/statistiques.php
+++ src/View/user/adminPanel/statistique/statistiques.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Votre Page</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="assets/javascript/statistiques.js"></script>
-</head>
-<body>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Votre Page</title>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="assets/javascript/statistiques.js"></script>
+
+
+<div class="HBox">
+    <div id="titleStat" class="title"><span>Statistiques</span></div>
+</div>
+
 <div class="VBox">
     <canvas id="test"></canvas>
 </div>
-</body>
-</html>
+
