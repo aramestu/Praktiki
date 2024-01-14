017c67045667febf458f690ffedd4671a842825a
schaz_ <mathias.lemoine@etu.umontpellier.fr>
2024-01-14 22:43:52 +0100

    fix: statistiques.php

--- src/View/user/adminPanel/statistique/statistiques.php
+++ src/View/user/adminPanel/statistique/statistiques.php
@@ -1,6 +1,3 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Votre Page</title>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="assets/javascript/statistiques.js"></script>
 
