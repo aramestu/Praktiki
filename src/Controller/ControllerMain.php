4f36450dfbb7faf4906361082bc7d4878435e5a5
francoisn <norman.francois@etu.umontpellier.fr>
2024-01-14 03:22:07 +0100

    fix [Acceptation Cookie] : modification du la durée de vie du cookie d'acceptation des cookies afin que celui ci ne soit pas demandé à chaque début de session

--- src/Controller/ControllerMain.php
+++ src/Controller/ControllerMain.php
@@ -120,7 +120,7 @@ class ControllerMain extends ControllerGenerique
      */
     public static function setCookie(): void
     {
-        Cookie::enregistrer('bannerClosed', true);
+        Cookie::enregistrer('bannerClosed', true,10 * 365 * 24 * 60 * 60);
         header('Location: frontController.php?action=home');
         exit();
     }
