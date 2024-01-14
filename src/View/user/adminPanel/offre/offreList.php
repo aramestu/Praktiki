94ee32ec9242fa9adc544b6f9507b7b867db4539
Soren Starck <soren.starck@etu.umontpellier.fr>
2024-01-14 22:30:13 +0100

    fix: removed popup

--- src/View/user/adminPanel/offre/offreList.php
+++ src/View/user/adminPanel/offre/offreList.php
@@ -34,14 +34,3 @@ use App\SAE\Controller\ControllerEntreprise;
     }
     ?>
 </div>
-
-<div id="popUpDelete" class="subContainer">
-    <a id="popUpDeleteClose"><img src="assets/images/close-icon.png" id="closeIcon" alt="Close"></a>
-    <div id="popUpDeleteContent">
-        <p>Êtes-vous sûr de vouloir supprimer cette offre ?</p>
-        <div class="HBox">
-            <a class="button popUpDeleteButton" id="popUpDeleteNo">Non</a>
-            <a class="button popUpDeleteButton" id="popUpDeleteYes" href="frontController.php?controller=ExpPro&action=supprimerOffre&experiencePro=<?php echo rawurlencode($expPro->getIdExperienceProfessionnel()) ?>">Oui</a>
-        </div>
-    </div>
-</div>
